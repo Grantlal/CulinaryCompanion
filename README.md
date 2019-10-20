c0e1373ea46f39c36b710254164ff334c8fb508b
Andrew Nekowitsch <anekowitsch@gmail.com>
2019-10-20 13:17:13 -0500

    Andrew (#14)
    * Update to material v0.1
    
    Added the html link
    updated readme
    updated v-on:click to c-on:click.native
    
    * Update README.md
    
    * drawer
    
    * fixed dependencies
    
    * Implemented a drawer
    
    * image background
    
    * Update App.vue

--- README.md
+++ README.md
@@ -1,39 +1,40 @@
-
 ### Getting Started
 
-- install Node.js / NPM
-https://nodejs.org/en/
+-   install Node.js / NPM
+    https://nodejs.org/en/
 
-- Clone Repository
+-   Clone Repository
 
-        git clone 
-        
-        cd springboot_vue
+          git clone
 
+          cd client
 
-### Installation  
-        
-- Build front-end environment
-You may or may not have to install uribuilder, Hopefully my package-lock change fixed that. Who knows though, keeping it for sanitys sake.
+### Installation
 
-        cd springboot_vue/frontend
+-   Build front-end environment
+    You may or may not have to install uribuilder, Hopefully my package-lock change fixed that. Who knows though, keeping it for sanitys sake.
 
-        npm install 
-        
-        npm install uribuilder
+            cd client
+
+            npm install
+
+            //if these are not installed with npm install
+            npm install uribuilder
+
+            npm install vue-material
 
 ### Back-end
-- Go here :) 
+
+-   Go here :)
+
 ```
 https://github.com/Grantlal/CulinaryCompanionAPI
 ```
 
+-   Run Front-end Web Page
 
-- Run Front-end Web Page
+          cd client
 
-        cd springboot_vue/frontend
+          npm run dev
 
-        npm run dev
-
-> You can also run `cd springboot_vue/frontend;npm run build` 
-
+> You can also run `cd springboot_vue/frontend;npm run build`
--- client/src/App.vue
+++ client/src/App.vue
@@ -1,128 +1,189 @@
 <template>
-    <div class="wrapper">
-        <db-header>
-        </db-header>
-        <el-row class="container">
-            <el-col :span="4" class="menu">
-                <db-sidebar></db-sidebar>
-            </el-col>
-            <el-col :span="20" class="content">
-                    <h4 v-model = "recipeExample">
-                    {{recipeExample}} </h4>  
-                    <button v-on:click='getRecipe()' class='mediumButtons'> Find CHICKEN </button>   
-            </el-col>
-        </el-row>
-        <el-row>
-            <el-col :span="24">
-            </el-col>
-        </el-row>
-    </div>
+  <div class="page-container" style="height:100%;">
+    <md-app>
+      <md-app-toolbar
+        class="md-primary"
+        style="background-image: linear-gradient(to right, rgb(229, 247, 228), rgb(201, 250, 197));"
+      >
+        <md-button class="md-icon-button" @click="toggleMenu" v-if="!menuVisible">
+          <md-icon>menu</md-icon>
+        </md-button>
+        <md-button class="md-icon-button" v-if="menuVisible">
+          <md-icon style="opacity: 0;">menu</md-icon>
+        </md-button>
+        <span id="title">Cullinary Companion</span>
+      </md-app-toolbar>
+
+      <md-app-drawer
+        :md-active.sync="menuVisible"
+        md-persistent="full"
+        style="background-image: linear-gradient(to bottom left, rgb(229, 247, 228), rgb(201, 250, 197)); max-height:100%;"
+      >
+        <md-toolbar class="md-transparent" md-elevation="0">
+          <span>Filters</span>
+
+          <div class="md-toolbar-section-end">
+            <md-button class="md-icon-button md-dense" @click="toggleMenu">
+              <md-icon>keyboard_arrow_left</md-icon>
+            </md-button>
+          </div>
+        </md-toolbar>
+
+        <md-list>
+          <md-list-item>
+            <md-checkbox v-model="opt1" value="1">Filter Option 1</md-checkbox>
+            <h1>{{opt1}}</h1>
+          </md-list-item>
+
+          <md-list-item>
+            <md-checkbox v-model="opt2" value="2">Filter Option 1</md-checkbox>
+            <h1>{{opt2}}</h1>
+          </md-list-item>
+
+          <md-list-item>
+            <md-checkbox v-model="opt3" value="3">Filter Option 1</md-checkbox>
+
+            <h1>{{opt3}}</h1>
+          </md-list-item>
+
+          <md-list-item>
+            <md-checkbox v-model="opt4" value="4">Filter Option 1</md-checkbox>
+
+            <h1>{{opt4}}</h1>
+          </md-list-item>
+        </md-list>
+      </md-app-drawer>
+
+      <md-app-content id="cont">
+        <h4>{{recipeExample}}</h4>
+        <md-button v-on:click.native="getRecipe()" class="md-raised">Find CHICKEN</md-button>
+      </md-app-content>
+    </md-app>
+  </div>
 </template>
 
 <script>
+import { UriBuilder } from "uribuilder";
 
-    import DbHeader  from './components/DbHeader.vue'
-    import DbSidebar from './components/DbSidebar.vue'
-    import DbFilterinput from './components/DbFilterinput.vue'
-    import DbTable from './components/DbTable.vue'
-    import DbFooter from './components/DbFooter.vue'
-    import ElRow from "element-ui/packages/row/src/row";
-    import { UriBuilder } from 'uribuilder';
+export default {
+  data() {
+    return {
+      array: [],
+      boolean: false,
+      string: null,
+      novalue: null,
+      disabled: true,
+      obj1: { name: "obj1" },
+      obj2: { name: "obj2" },
+      obj: null,
+      indeterminate: true,
 
-    export default {
-        data() {
-            return {
-                recipeExample: null,
-                recipe1: null,
-            }
-        },
-        methods: {
-            getRecipe: async function(event) {
-                try {
-                    //Saving this to know this call has worked :)
-                    // https://api.edamam.com/search?q=chicken&app_id=9a0c84a3&app_key=45bb00840fe3a634d119f86ff069c199
-                    const url = `http://localhost:8080/recipes`;
-                    const response = await fetch(url)
-                    .then((resp) => resp.json());
-                    console.log(response);
-                    this.recipeExample = response.firstRecipe;
-                    return response.firstRecipe;
-                } catch (error) {
-                console.error(error);
-                }
-            }
-        },
-        name: 'app',
-        components: {
-            ElRow, DbHeader,
-            DbSidebar,
-            DbFilterinput,
-            DbTable,
-            DbFooter
-        },
+      recipeExample: null,
+      recipe1: null,
+      menuVisible: false
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.menuVisible = !this.menuVisible;
+    },
+    getRecipe: async function(event) {
+      try {
+        //Saving this to know this call has worked :)
+        // https://api.edamam.com/search?q=chicken&app_id=9a0c84a3&app_key=45bb00840fe3a634d119f86ff069c199
+        const url = `http://localhost:8080/recipes`;
+        const response = await fetch(url).then(resp => resp.json());
+        console.log(response);
+        this.recipeExample = response.firstRecipe;
+        return response.firstRecipe;
+      } catch (error) {
+        this.recipeExample = "Error connecting to database.";
+        console.error(error);
+      }
     }
+  },
+  name: "app",
+  components: {}
+};
 </script>
 
 <style>
+#title {
+  min-height: 70px;
+  font-size: 50px;
+  line-height: 70px;
+}
+.md-checkbox {
+  display: flex;
+}
 
-    .mediumButtons {
-    }
+table {
+  width: 100%;
+  table-layout: fixed;
 
-    element.style {
-        background-color: rgb(10, 47, 88);
-    }
+  th {
+    text-align: left;
+  }
+}
+.md-app {
+  min-height: 350px;
+  border: 1px solid rgba(#000, 0.12);
+  width: 100%;
+  height: 100%;
+}
 
-    body {
-        font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
-        margin: 0;
-        display: flex;
-        min-height: 100vh;
-        flex-direction: column;
-    }
+.md-drawer {
+  width: 230px;
+  max-width: calc(100vw - 125px);
+}
 
-    .el-menu, body, html {
-        height: 100%;
-    }
 
-    .wrapper {
 
-        position: relative;
-    }
+.el-menu,
+body,
+html {
+  height: 100%;
+}
 
-    footer, div {
-        display: block;
-    }
+.wrapper {
+  position: relative;
+}
 
-    .container {
-        padding-top: 70px;
-        flex: 1;
-    }
+footer,
+div {
+  display: block;
+}
 
-    .container, .wrapper {
-        height: 100%;
-    }
+.container {
+  padding-top: 70px;
+  flex: 1;
+}
 
-    .menu {
-        height: 100%;
-        background-color: #eef1f6;
-    }
+.container,
+.wrapper {
+  height: 100%;
+}
 
-    .content {
-        padding-top: 25px;
-        padding-right: 25px;
-        padding-bottom: 125px;
-        padding-left: 25px;
-    }
+.menu {
+  height: 100%;
+  background-color: #eef1f6;
+}
 
-    .footer {
-        height: 120px;
-        background-color: #324057;
-        color: #a4aebd;
-        width: 100%;
-        z-index: 1000;
-        margin-top: -120px;
-        line-height: 1;
-        font-size: 22px;
-    }
+.content {
+  padding-top: 25px;
+  padding-right: 25px;
+  padding-bottom: 125px;
+  padding-left: 25px;
+}
 
+.footer {
+  height: 120px;
+  background-color: #324057;
+  color: #a4aebd;
+  width: 100%;
+  z-index: 1000;
+  margin-top: -120px;
+  line-height: 1;
+  font-size: 22px;
+}
 </style>
